8ee02d8aef41cee4456e8829ac93c32dd8eb2cfd
PrimePrices <104781190+PrimePrices@users.noreply.github.com>
2024-03-13 10:06:29 +0000

    admin_bp might be working now

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,9 +1,5 @@
 from flask import Blueprint
-
-admin_bp = Blueprint('admin', __name__)
-
-# Import routes to register them
-from . import routes
+from .routes import admin_bp
 
 def init_admin_app(app):
     app.register_blueprint(admin_bp, url_prefix='/admin')
