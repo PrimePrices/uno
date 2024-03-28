fd72f95c2fd7dc1128de81e3abda7964e22d3d28
PrimePrices <104781190+PrimePrices@users.noreply.github.com>
2024-03-28 14:10:44 +0000

    Added default values for SQL

--- uno/create.sql
+++ uno/create.sql
@@ -4,15 +4,15 @@ CREATE TABLE IF NOT EXISTS hands (
   game_id INTEGER,
   cards TEXT,
   username TEXT,
-  number_of_cards INT
+  number_of_cards INT DEFAULT 7
 );
 CREATE TABLE IF NOT EXISTS games(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  rules TEXT,
-  number_of_players INTEGER,
+  rules TEXT DEFAULT "",
+  number_of_players INTEGER DEFAULT 1,
   players TEXT,
   next_player TEXT,
-  direction INTEGER,
+  direction INTEGER DEFAULT 0, 
   discard TEXT,
   draw TEXT
 )
